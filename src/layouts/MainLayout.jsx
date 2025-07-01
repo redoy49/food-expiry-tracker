47b37a3b89953604ea58e0e2ac1b435697bba591
Redoy Al Hasan <mdredoyhasan49@gmail.com>
2025-07-01 15:41:42 +0600

    Make navbar and footer full width

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,52 @@
+// import React from "react";
+// import Footer from "../components/Footer";
+// import { Outlet } from "react-router";
+// import Home from "../pages/Home/Home";
+// import Header from "../components/Header";
+// import Navbar from "../components/Navbar";
+
+// const MainLayout = () => {
+//   return (
+//     <div className="max-w-[1280px] mx-auto min-h-screen">
+//       <Navbar />
+
+//       <main className="flex-grow py-8">
+//         <Outlet />
+//       </main>
+
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default MainLayout;
+
 import React from "react";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router";
-import Home from "../pages/Home/Home";
-import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-[1280px] mx-auto min-h-screen">
-      <Navbar />
+    <div className="min-h-screen flex flex-col">
+      {/* Full-width Navbar background */}
+      <div className="bg-success text-success-content w-full">
+        <div className="max-w-[1600px] mx-auto">
+          <Navbar />
+        </div>
+      </div>
 
-      <main className="flex-grow py-8">
+      {/* Page Content */}
+      <main className="flex-grow py-8 max-w-[1600px] mx-auto">
         <Outlet />
       </main>
 
-      <Footer />
+      {/* Full-width Footer background */}
+      <div className="bg-success text-success-content w-full">
+        <div className="max-w-[1600px] mx-auto">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
